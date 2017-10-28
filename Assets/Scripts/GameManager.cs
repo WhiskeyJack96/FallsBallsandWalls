fe375a0818652f3baaa63f1bfc977db2dbf2f8a9
WhiskeyJack <jacob.mikesell96@gmail.com>
2017-10-27 22:43:16 -0700

    Created GameManager
    Created a manager for spawning and clearing floor tiles

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public List<GameObject> floor = new List<GameObject>(15);
+	private Queue<GameObject> floorTiles = new Queue<GameObject>(floor);
+	public Camera cam;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//for each element in the list
+			//if the element is not visible
+				// remove it from the list
+		if(true) //get from check camera
+		float xshift = cam.ViewportToWorldPoint(new Vector3(1,.5f,0)).x;
+		//Debug.Log(xshift);
+		
+	}
+}
