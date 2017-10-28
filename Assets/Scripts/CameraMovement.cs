bbd2aeed5c1ab61bcee915edf20c008851109127
Sds <ace.lowder@gmail.com>
2017-10-27 22:40:06 -0700

    Camera/Bound Checking Scripts
    Finished Camera Movement Script
    Finished Offscreen Check Script

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	private float accel = 0.00005f;
+	private float vel = 0.005f;
+
+	void FixedUpdate() {
+		// Increase x pos
+		float posX = transform.position.x + vel;
+
+		// Increase vel change each update
+		vel += accel;	
+
+		// Update camera pos
+		transform.position = new Vector3(
+								 	posX,
+									transform.position.y,
+									transform.position.z
+								 );		
+	}
+
+}
---
+++ Assets/Scripts/OffscreenCheck.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffscreenCheck : MonoBehaviour {
+	
+	void FixedUpdate() {
+		CheckInBounds();
+	}
+
+	bool CheckInBounds() {
+		float safeDistance = GetSafeDistance();
+		if (transform.position.x >= safeDistance) {
+			return true;
+		} else {
+			print ("OUT OF BOUNDS");
+			return false;
+		}
+	}
+
+	float GetSafeDistance() {
+		float maxCamDistance = GetMaxCamDistance();
+		float objDistance = 0f;	//Object width
+		return maxCamDistance + objDistance;
+	}
+
+	float GetMaxCamDistance() {
+		Camera cam = Camera.main;
+		Vector3 camPos = cam.gameObject.transform.position;
+		float camHeight = 2f * cam.orthographicSize;
+		float camWidth = camHeight * cam.aspect;
+		return camPos.x - (camWidth / 2);
+	}
+
+}
