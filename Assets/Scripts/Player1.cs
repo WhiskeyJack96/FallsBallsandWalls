14008ddb36bd5e794181f4b7b445b372a34ec0c9
WendyWjt <jiangtow@uci.edu>
2017-10-27 23:08:29 -0700

    Player1 Controller [unfinished]

---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player1 : MonoBehaviour{
+        private float v_MaxSize = 50f;                    // The maximum radius of the ball.
+        private float v_MinSize = 5f;                     // The minimum radius of the ball.
+        private Rigidbody2D FallBall;                     // The ball
+
+        //KeyCode key_Increase = KeyCode.W;                  // Press W to increase the size
+        //KeyCode key_Decrease = KeyCode.S;                  // Press S to increase the size  
+
+        // Use this for initialization
+        void Start(){
+
+        }
+
+    // Update is called once per frame
+        void Update(){
+        // Widen the object by 0.1
+        //if (Input.GetKeyDown("W")){
+            //Event e = Event.current;
+            //if (){
+                FallBall.transform.localScale = new Vector3(0.1f, 0.1f, 0);
+            //}
+        //}
+        }
+}
