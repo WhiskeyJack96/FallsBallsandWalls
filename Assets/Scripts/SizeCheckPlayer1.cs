85cdd1313cf311a31e11f46cc9a6eecb26bd25ee
mattinthecat <phoisgood1@gmail.com>
2017-10-28 21:26:12 -0700

    Bug fix for ball growing in cave

---
+++ Assets/Scripts/SizeCheckPlayer1.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeCheckPlayer1 : MonoBehaviour {
+
+    public Rigidbody2D FallBall;
+    public Vector3 temp_max = new Vector3(0, 0, 27);
+    public LayerMask mask = -1;
+    //
+    //
+    private void Awake()
+    {
+       
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(0, 1), .1f, mask);
+
+
+        
+        if (hit.collider != null)
+        {
+            print(temp_max);
+            
+            if (FallBall.transform.localScale.y > temp_max.y)
+            {
+                if (temp_max.z == 27)
+                {
+                    temp_max = FallBall.transform.localScale;
+
+                }
+                FallBall.transform.localScale = temp_max;
+            }
+            
+
+        }
+        else
+        {
+            temp_max = new Vector3(0, 0, 27);
+        }
+	}
+}
