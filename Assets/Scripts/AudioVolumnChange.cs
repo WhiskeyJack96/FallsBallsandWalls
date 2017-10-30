803756387028652094f8dec96dd5fa7ec8d11fd8
WendyWjt <jiangtow@uci.edu>
2017-10-29 19:39:51 -0700

    UI text color and Fix problem about slider for music volumn

---
+++ Assets/Scripts/AudioVolumnChange.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AudioVolumnChange : MonoBehaviour
+{
+    public void AudioChange(float newVolumn)
+    {
+        UnityEngine.AudioListener.volume = newVolumn;
+    }
+
+}
