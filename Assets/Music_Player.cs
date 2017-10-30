2e4e2ee372bbd646335040edb3cf6302622a6b4b
mattinthecat <phoisgood1@gmail.com>
2017-10-29 22:16:48 -0700

    Music Loop

---
+++ Assets/Music_Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music_Player : MonoBehaviour
+{
+    public double length_in_seconds = 46.210;
+    public AudioClip clip;
+    private double nextEventTime;
+    private int flip = 0;
+    private AudioSource[] audioSources = new AudioSource[2];
+    private bool running = false;
+    void Start()
+    {
+        int i = 0;
+        while (i < 2)
+        {
+            GameObject child = new GameObject("Player");
+            child.transform.parent = gameObject.transform;
+            audioSources[i] = child.AddComponent<AudioSource>();
+            i++;
+        }
+        nextEventTime = AudioSettings.dspTime + 2.0F;
+        running = true;
+    }
+    void Update()
+    {
+        if (!running)
+            return;
+
+        double time = AudioSettings.dspTime;
+        if (time + 1.0F > nextEventTime)
+        {
+            audioSources[flip].clip = clip;
+            audioSources[flip].PlayScheduled(nextEventTime);
+            Debug.Log("Scheduled source " + flip + " to start at time " + nextEventTime);
+            nextEventTime += length_in_seconds;
+            flip = 1 - flip;
+        }
+    }
+}
