77aedec89606861aa4be4fc8d7872633a04b14f1
WhikseyJack <jacob.mikesell96@gmail.com>
2017-10-29 18:24:43 -0700

    added audio effect and a ground check

---
+++ Assets/GroundCheck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour {
+
+	public LayerMask mask = -1;
+	public AudioSource roll;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(0, -1), .1f, mask);
+
+
+        
+        if (hit.collider == null)
+        {
+        	roll.mute = true;
+        }
+        else{
+        	roll.mute = false;
+        }
+	}
+}
